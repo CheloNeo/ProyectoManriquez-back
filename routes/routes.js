890121822d01c23f84e3314fb9415a8f89b6c278
Juan Larenas <jalarenasv@gamil.com>
2022-01-19 00:19:34 -0300

    feat(rutas):Add rutes sales

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,15 @@ const { Router } = require('express');
 const router = Router();
 let controller_User = require('../controllers/control-User');
 
+//login
 router.post('/new', controller_User.creacionUser);
 router.post('/login', controller_User.login);
 router.post('/validate', controller_User.validateToken);
 router.delete('/delete/:rut',controller_User.deleteUsuario);
 
+//ventas
+router.post('/newGuiaDeVenta', controller_User.creacionUser);
+router.post('/newOrdenDeCompra', controller_User.creacionUser);
+router.get('/filtrarVenta/:id', controller_User.creacionUser);
+router.get('/verificarEstado/:id', controller_User.login);
 module.exports = router;
