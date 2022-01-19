485b15fe617c80bac3e040a2b38a81d42a68014c
CheloNeo <maemguitarra@gmail.com>
2022-01-19 00:24:42 -0300

    feat(controlVentas): create-model-client

---
+++ models/Cliente.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+const ClienteSchema = Schema({
+    nombre: String,
+    apellidos: String,
+    direccion: String,
+    telefono: String,
+    correo: String,
+    rut: String,
+});
+
+
+module.exports = model('Cliente', ClienteSchema);
