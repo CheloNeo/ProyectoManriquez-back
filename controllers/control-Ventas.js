cc55182e73c504a27a4635310b499c3702e1efb3
CheloNeo <maemguitarra@gmail.com>
2022-01-19 01:44:07 -0300

    feat(control):add-method-crearOrden

--- controllers/control-Ventas.js
+++ controllers/control-Ventas.js
@@ -1,4 +1,5 @@
 let controller = {}
+const Orden = require('../models/OrdenDeVenta');
 let Venta = require('../models/Ventas');
 
 controller.crearVenta = (req,res)=>{
@@ -13,9 +14,24 @@ controller.crearVenta = (req,res)=>{
     .catch((err)=>{
         res.json({status:500,mensaje:"Ingreso fallido revisa los campos"});
     })
-
 }
 
+controller.crearOrden = (req,res)=>{
+    //crearemos la oden de compra
+    try {
+        const orden = new Orden(req.body);
+        orden.save()
+        .then((data)=>{
+            res.json({status:200,mensaje:"Orden  guardada de manera exitosa!"});
+        })
+        .catch((err)=>{
+            res.json({status:500,mensaje:"Orden fallida!"});
+        })
+    } catch (error) {
+        res.json({status:500,mensaje:"Creacion de orden fallida!"});
+    }
+
+}
 
 
 
