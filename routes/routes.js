d1630ffe188a3369872d3c2fa5a575baa519c34e
Juan Larenas <jalarenasv@gamil.com>
2022-01-19 00:29:15 -0300

    refactor(routes): add controllers-ventas

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 let controller_User = require('../controllers/control-User');
+let controller_Ventas = require('../controllers/control-Ventas')
 
 //login
 router.post('/new', controller_User.creacionUser);
@@ -9,8 +10,8 @@ router.post('/validate', controller_User.validateToken);
 router.delete('/delete/:rut',controller_User.deleteUsuario);
 
 //ventas
-router.post('/newGuiaDeVenta', controller_User.creacionUser);
-router.post('/newOrdenDeCompra', controller_User.creacionUser);
-router.get('/filtrarVenta/:id', controller_User.creacionUser);
-router.get('/verificarEstado/:id', controller_User.login);
+router.post('/newGuiaDeVenta', controller_Ventas);
+router.post('/newOrdenDeCompra',controller_Ventas);
+router.get('/filtrarVenta/:id',controller_Ventas);
+router.get('/verificarEstado/:id', controller_Ventas);
 module.exports = router;
