ccc86c485be0e9f3d155debe67643ba987b931ad
CheloNeo <maemguitarra@gmail.com>
2022-01-18 23:39:42 -0300

    refactor(control):rename file of controller_User

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const router = Router();
-let controller = require('../controllers/control');
+let controller_User = require('../controllers/control-User');
 
-router.post('/new', controller.creacionUser);
-router.post('/login', controller.login);
-router.post('/validate', controller.validateToken);
-router.delete('/delete/:rut',controller.deleteUsuario);
+router.post('/new', controller_User.creacionUser);
+router.post('/login', controller_User.login);
+router.post('/validate', controller_User.validateToken);
+router.delete('/delete/:rut',controller_User.deleteUsuario);
 
 module.exports = router;
