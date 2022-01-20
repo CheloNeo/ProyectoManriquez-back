1694b0095e4b6a933fc3e4762613e2b716991809
Juan Larenas <jalarenasv@gamil.com>
2022-01-19 23:03:21 -0300

    feat(controllers): modify message max

--- controllers/control-Cliente.js
+++ controllers/control-Cliente.js
@@ -20,14 +20,14 @@ controller.crearCliente = (req, res) => {
     }
 }
 
-controller.mensajeMasivo = async (req,res)=>{
+controller.mensajeMasivo = async (req, res) => {
 
-    const {asunto, mensaje} = req.body;
+    const { asunto, mensaje } = req.body;
 
     try {
 
         //primero traemos los correos de los clientes de nuestro sistema
-        
+
 
 
 
@@ -39,22 +39,23 @@ controller.mensajeMasivo = async (req,res)=>{
             port: 465,
             secure: true, // true for 465, false for other ports
             auth: {
-              user: 'maemguitarra@gmail.com', // usuario de correo
-              pass: 'snshioiwejtyenwq', // clave de acceso a gmail
+                user: 'maemguitarra@gmail.com', // usuario de correo
+                pass: 'snshioiwejtyenwq', // clave de acceso a gmail
             },
         });
-    
+
         let info = await transporter.sendMail({
             from: '"Pisos Manriquez " <maemguitarra@gmail.com>', // sender address
-            to: ['juan.larenas@alumnos.uv.cl','marcelo.estay@alumnos.uv.cl'], // list of receivers
+            to: ['juan.larenas@alumnos.uv.cl', 'marcelo.estay@alumnos.uv.cl'], // list of receivers
             subject: `${asunto}`, // Subject line
             text: "", // plain text body
             html: `${mensaje}` // html body
         });
-        res.json({status:200,mensaje:"mensaje enviado con exito!"})
+        res.json({ status: 200, mensaje: "mensaje enviado con exito!" })
     } catch (error) {
-        res.json({status:500,mensaje:"el mensaje tuvo fallas al momento de enviar intente mas tarde!"})
-}}
+        res.json({ status: 500, mensaje: "el mensaje tuvo fallas al momento de enviar intente mas tarde!" })
+    }
+}
 controller.verClientes = async (req, res) => {
     try {
         const clientes = await Cliente.find();
