cb504f8e4e603dbc48b492768fed9d7e4db0b083
CheloNeo <maemguitarra@gmail.com>
2022-01-19 22:57:11 -0300

    feat(control):add-methods-cruds-proveedor

--- controllers/control-Proveedor.js
+++ controllers/control-Proveedor.js
@@ -24,4 +24,60 @@ controller.crearProveedor = (req,res)=>{
     }
 }
 
+//eliminar proveedor
+controller.eliminarProveedor = (req,res)=>{
+    try {
+        //eliminamos el proveedor
+        Proveedor.findOneAndDelete(req.body)
+        .then(()=>{
+            res.json(
+                {
+                    status:200,
+                    mensaje:"eliminado con exito!"
+                }
+            )
+        })
+        .catch((err)=>{
+            res.json({
+                status:500,
+                mensaje:"Elminado con exito!"
+            })
+        })
+
+
+    } catch (error) {
+        res.json({
+            status:500,
+            mensaje:"Elminado con exito!"
+        })
+    }
+}
+
+
+//lista de proveedores
+controller.ListarProveedores = (req,res)=>{
+    try {
+        
+        Proveedor.find({})
+        .then((data)=>{
+            res.json({
+                status:200,
+                proveedores:data
+            })
+        })
+        .catch((err)=>{
+            res.json({
+                status:500,
+                mensaje:"No se pudo crear esta lista de proveedores"
+            })
+        })
+
+    } catch (error) {
+        res.json({
+            status:500,
+            mensaje:"No se pudo crear esta lista de proveedores"
+        })
+    }
+}
+
 module.exports = controller
