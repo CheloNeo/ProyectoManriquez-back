1d9fbd5eb7496f9313bbf6f0efd73870e00d2fec
Juan Larenas <jalarenasv@gamil.com>
2022-01-20 22:58:50 -0300

    feat(routes): add routes products

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 let controller_User = require('../controllers/control-User');
-let controller_Ventas = require('../controllers/control-Ventas')
+let controller_Ventas = require('../controllers/control-Ventas');
+let controller_Productos = require('../controllers/control-Productos');
+
 
 //login
 router.post('/new', controller_User.creacionUser);
@@ -14,4 +16,10 @@ router.post('/newGuiaDeVenta', controller_Ventas.crearVenta);
 router.post('/newOrdenDeCompra',controller_Ventas.crearOrden);
 router.get('/filtrarVenta/:id',controller_Ventas.filtrarVenta);
 router.get('/verificarEstado/:id', controller_Ventas.verificarEstado);
+
+//productos
+router.post('/newProducto', controller_Productos.crearProducto);
+router.put('/modificarProducto/:id', controller_Productos.modificarProducto);
+router.delete('/eliminarProducto/:id', controller_Productos.eliminarProducto);
+
 module.exports = router;
