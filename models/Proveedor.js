4dfbd394c5fbb1d65b63b50e901fb1368a9be7d2
Juan Larenas <jalarenasv@gamil.com>
2022-03-03 16:01:01 -0300

    feat(proveedores): model add rut

--- models/Proveedor.js
+++ models/Proveedor.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require("mongoose");
 const ProveedorSchema = Schema({
     nombre: String,
+    rut: String,
     nombreContacto: String,
     direccion: String,
     telefono: String,
