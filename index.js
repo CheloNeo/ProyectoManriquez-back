67f2cfc3ab1a8f4969c95667b6eab6a8b0c6517d
CheloNeo <maemguitarra@gmail.com>
2022-03-26 22:47:16 -0300

    deploy2

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let app = express();
 // Proteccion de rutas
 
     let corsOptions = {
-        // origin:'https://pisos-manriquez-16de3.web.app/',
-        origin:'http://localhost:4200/',
+        origin:'https://pisos-manriquez-984b2.web.app/',
+        // origin:'http://localhost:4200/',
         optionsSuccessStatus: 200,
         methods: "GET , PUT , POST , DELETE"
     };
